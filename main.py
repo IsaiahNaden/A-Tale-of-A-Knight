9204f308e2a0fcd68cfc331f310130cd4cdd350e
Isaiah Naden <isaiahnaden22@gmail.com>
2024-04-24 21:42:08 +0800

    created new branch and create game window.

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from sprites import *
 from config import *
 import sys
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((screen_width, screen_height))
-        self.clock = pygame.time.Clock()
-        self.font = pygame.font.Font('Arial', 32)
-        self.player = pygame.draw.rect((250, 0, 0))
-        self.running = True
+pygame.init()
 
-    def new(self):
-        self.playing = True
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+run = True 
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
