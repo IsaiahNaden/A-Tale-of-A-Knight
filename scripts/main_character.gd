caf0f8c0eb38e570478a145447938069aaedb7c8
Isaiah Naden <isaiahnaden22@gmail.com>
2024-05-15 23:40:38 +0800

    added world & healthbar

---
+++ scripts/main_character.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(_delta):
+	# Add the gravity.
+	
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	var Direction = Input.get_axis("ui_up", "ui_down")
+	if direction:
+		velocity.x = direction * SPEED
+		velocity.y = Direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("Portal"):
+		position.x = 0
+		position.y = 23
