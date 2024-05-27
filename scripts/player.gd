ac3cee67bc2072a1191bb73d86df6edd44ab04d4
avinaeshk06 <avinaeshk06@gmail.com>
2024-05-28 00:43:37 +0800

    added updating healthbars for enemy and player

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var enemy_inattack_range = false
 var enemy_attack_cooldown = true
-var health = 250
+var health = 100
 var player_alive = true
 
 var attack_ip = false
@@ -16,6 +16,7 @@ func _physics_process(delta):
 	enemy_attack()
 	attack()
 	current_camera()
+	update_health()
 	
 	if health <= 0:
 		player_alive = false  #add respawn screen
@@ -146,3 +147,23 @@ func current_camera():
 		$cliffside_camera.enabled = true
 		
 		
+		
+		
+func update_health():
+	var healthbar = $healthbar
+	
+	healthbar.value = health
+	
+	if health >=100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+
+
+func _on_regen_timer_timeout():
+	if health < 100:
+		health = health + 20
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
